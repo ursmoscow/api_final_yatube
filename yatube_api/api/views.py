845b9cd756aaec9b6af67a3c22ab103ae909197c
ursmoscow <61637040+ursmoscow@users.noreply.github.com>
2023-06-06 14:18:54 +0300

    tests_ok_2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets, filters, mixins, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly,
     IsAuthenticated,
